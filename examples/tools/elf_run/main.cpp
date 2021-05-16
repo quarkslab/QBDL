361dc2a5e87f57d554b10849df7c239aa2188bd2
Adrien Guinet <adrien@guinet.me>
2021-05-16 16:34:20 +0200

    elf_run: simplify + windows compatibility
    * make elf_run runs under Windows: only support the "puts" symbol. This
      indeed needs ABI wrappers to work in the general case!
    * simplify: only load imported libraries once!

--- examples/tools/elf_run/main.cpp
+++ examples/tools/elf_run/main.cpp
@@ -2,9 +2,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <stdarg.h>
+
+#ifndef _WIN32
 #include <dlfcn.h>
 #include <unistd.h>
-#include <stdarg.h>
+#endif
 
 #include <LIEF/LIEF.hpp>
 #include <QBDL/Engine.hpp>
@@ -13,53 +16,55 @@
 
 using namespace QBDL;
 
-std::unordered_map<std::string, void *> dlmap;
-int __android_log_print(int, const char *tag, const char *msg, ...);
-
-static std::unordered_map<std::string, uintptr_t> SYMS{
-    {"getpid", reinterpret_cast<uintptr_t>(&::getpid)},
-    {"printf", reinterpret_cast<uintptr_t>(&::printf)},
-    {"__android_log_print", reinterpret_cast<uintptr_t>(&__android_log_print)}};
-
-int __android_log_print(int, const char *tag, const char *msg, ...) {
+extern "C" {
+#ifndef _WIN32
+static int __android_log_print(int, const char *tag, const char *msg, ...) {
   va_list args;
   va_start(args, msg);
   vprintf(msg, args);
   va_end(args);
   return 0;
 }
+#endif
 
+#ifdef _WIN32
+__attribute__((sysv_abi)) extern "C" int sysv_puts(const char* str) {
+  return puts(str);
+}
+#endif
+}
+
+static std::unordered_map<std::string, uintptr_t> SYMS{
+#ifndef _WIN32
+    {"__android_log_print", reinterpret_cast<uintptr_t>(&__android_log_print)},
+#endif
+#ifdef _WIN32
+    {"puts", reinterpret_cast<uintptr_t>(&sysv_puts)}
+#endif
+};
 
 namespace {
 
 struct FinalTargetSystem: public Engines::Native::TargetSystem {
   using Engines::Native::TargetSystem::TargetSystem;
 
-  uint64_t symlink(Loader &loader, const LIEF::Symbol &sym) override {
-    auto &elfLoader = reinterpret_cast<Loaders::ELF &>(loader);
+  uint64_t symlink(Loader &, const LIEF::Symbol &sym) override {
     const std::string &name = sym.name();
     auto it_sym = SYMS.find(name);
     if (it_sym != std::end(SYMS)) {
       return it_sym->second;
     }
 
-    for (const std::string &lib :
-        elfLoader.get_binary().imported_libraries()) {
-      auto res = dlmap.insert({lib, dlopen(lib.c_str(), RTLD_NOW)});
-      void *hdl = res.first->second;
-      if (hdl == nullptr) {
-        fprintf(stderr, "Can't load %s\n", dlerror());
-        return 0;
-      }
-      void *symAddr = dlsym(hdl, name.c_str());
-      if (symAddr == nullptr) {
-        continue;
-      }
-      return reinterpret_cast<uintptr_t>(symAddr);
+#ifndef _WIN32
+    void *symAddr = dlsym(RTLD_DEFAULT, name.c_str());
+    return reinterpret_cast<uint64_t>(symAddr);
+#else
+    void* symAddr = nullptr;
+#endif
+    if (symAddr == nullptr) {
+      fprintf(stderr, "Can't resolve %s\n", name.c_str());
     }
-
-    fprintf(stderr, "Can't resolve %s\n", name.c_str());
-    return 0;
+    return reinterpret_cast<uint64_t>(symAddr);
   }
 };
 
@@ -76,8 +81,28 @@ int main(int argc, char **argv) {
 
   const char *path = argv[1];
 
-  std::unique_ptr<Loaders::ELF> loader = Loaders::ELF::from_file(
-      path, *system, Loader::BIND::NOW);
+  auto bin = LIEF::ELF::Parser::parse(path);
+  if (!bin) {
+    fprintf(stderr, "Unable to parse binary!\n");
+    return 1;
+  }
+#ifndef _WIN32
+  // dlopen every imported libraries
+  for (const std::string &lib:
+      bin->imported_libraries()) {
+    const char* libname = lib.c_str();
+    void* hdl = dlopen(lib.c_str(), RTLD_NOW);
+    if (hdl == nullptr) {
+      fprintf(stderr, "Warning: can't load library %s: %s\n", libname, dlerror());
+    }
+    else {
+      fprintf(stderr, "Loaded imported library %s...\n", libname);
+    }
+  }
+#endif
+
+  std::unique_ptr<Loaders::ELF> loader = Loaders::ELF::from_binary(std::move(bin),
+      *system, Loader::BIND::NOW);
   if (!loader) {
     fprintf(stderr, "unable to load binary!\n");
     return EXIT_FAILURE;
@@ -85,7 +110,7 @@ int main(int argc, char **argv) {
   auto main =
       reinterpret_cast<int (*)(int, char **)>(loader->get_address("main"));
   if (main == nullptr) {
-    fprintf(stderr, "Can't find symbol 'find'\n");
+    fprintf(stderr, "Can't find symbol 'main'\n");
     return 1;
   }
   return main(argc - 1, &argv[1]);
